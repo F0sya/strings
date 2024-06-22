9f49133dab582696a40af3ee86d0cbab372b944b
F0sya <nikitachmyria@gmail.com>
2024-06-22 16:21:50 +0300

    First practice exercise is done.

--- strings/main.cpp
+++ strings/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+
+void space_to_tab(char* str) {
+	int i = 0;
+	do {
+		if (str[i] == ' ') {
+			str[i] = '\t';
+		}
+		i++;
+	} while (str[i] != '\0');
+}
+void printStr(char* str) {
+	int i = 0;
+	do {
+		cout << str[i];
+		i++;
+	} while (str[i] != '\0');
+}
+
 int main() {
+	char str[] = "Hello world!";
+	cout << "String before changes:\n";
+	printStr(str);
+	cout << "\nString after changes:\n";
+	space_to_tab(str);
+	printStr(str);
 	return 0;
 }
