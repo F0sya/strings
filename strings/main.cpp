f098cecd0a37c39c02e30586897cf62083d7c39e
F0sya <nikitachmyria@gmail.com>
2024-06-22 17:13:04 +0300

    Fourth practice exercise is done.

--- strings/main.cpp
+++ strings/main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+	char str[] = "Hello olleH";
+	bool checker = true;
+	int last_index = strlen(str) - 1;
+	for (int i = 0; i != last_index; i++) {
+		if (str[i] != str[last_index]) {
+			checker = false;
+			break;
+		}
+		last_index--;
+	}
+	(checker == true) ? cout << "Palindrome" : cout << "Not palindrome";
 	return 0;
 }
