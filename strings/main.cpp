78b6995489da04f2f90f1a6df784c0ee4eb04f03
F0sya <nikitachmyria@gmail.com>
2024-06-22 17:25:50 +0300

    First homework exercise is done.

--- strings/main.cpp
+++ strings/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+
+void deleter(char* str, int index) {
+	char new_str[] = "";
+	for (int i = index; i < strlen(str); i++) {
+		str[i] = str[i + 1];
+	}
+
+}
+void printStr(char* str) {
+	int i = 0;
+	do {
+		cout << str[i];
+		i++;
+	} while (str[i] != '\0');
+}
+
 int main() {
+	char str[] = "Hello world!";
+	cout << "String before changes:\n";
+	printStr(str);
+	int index; cout << "\nEnter an index to delete:"; cin >> index;
+	cout << "\nString after changes:\n";
+	deleter(str, index);
+	printStr(str);
 	return 0;
 }
