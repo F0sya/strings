3c0bc7c126d37ebb7fa3272c5327f6a1cf725aae
F0sya <nikitachmyria@gmail.com>
2024-06-22 17:10:00 +0300

    Third practice exercise is done.

--- strings/main.cpp
+++ strings/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main() {
+	char str[] = "HEllo world! world";
+	int words = 0;
+
+	int length = strlen(str);
+	
+	for (int i = 0; i < length; i++) {
+		if (str[i] == ' ') {
+			words++;
+		}
+	}
+	words++;
+	cout << "Amount of words: " << words << endl;
 	return 0;
 }
