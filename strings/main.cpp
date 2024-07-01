dc06d72fee50f2099c8801d903fc6f0dcfd00deb
F0sya <nikitachmyria@gmail.com>
2024-07-01 14:51:52 +0300

    Fourth homework exercise is done.

--- strings/main.cpp
+++ strings/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+
+void changer(char* str) {
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == '.') {
+			str[i] = '!';
+		}
+	}
+}
+void printStr(char* str) {
+	int i = 0;
+	do {
+		cout << str[i];
+		i++;
+	} while (str[i] != '\0');
+}
+
 int main() {
+	char str[] = "Hello. world....";
+	cout << "String before changes:\n";
+	printStr(str);
+	cout << "\nString after changes:\n";
+
+	changer(str);
+	printStr(str);
 	return 0;
 }
