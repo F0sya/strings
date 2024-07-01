a6adfed3499a8eb07b55c541e8498e4871f06461
F0sya <nikitachmyria@gmail.com>
2024-07-01 14:38:44 +0300

    Second homework exercise is done.

--- strings/main.cpp
+++ strings/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+
+void deleter(char* str, char symbol) {
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] != symbol) {
+			cout << str[i];
+		}
+	}
+}
+void printStr(char* str) {
+	int i = 0;
+	do {
+		cout << str[i];
+		i++;
+	} while (str[i] != '\0');
+}
+
 int main() {
+	char str[] = "Hello world!";
+	cout << "String before changes:\n";
+	printStr(str);
+	char symbol; cout << "\nEnter an symbol to delete:"; cin >> symbol;
+	cout << "\nString after changes:\n";
+	
+	deleter(str, symbol);
 	return 0;
 }
